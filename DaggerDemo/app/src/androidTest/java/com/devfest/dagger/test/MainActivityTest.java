ca09c7f6e30e67171312680240eab2654b77ea99
David Truxall <davetrux@gmail.com>
2014-05-05 21:23:02 -0400

    Functional Unit Test

--- DaggerDemo/app/src/androidTest/java/com/devfest/dagger/test/MainActivityTest.java
+++ DaggerDemo/app/src/androidTest/java/com/devfest/dagger/test/MainActivityTest.java
@@ -1,16 +1,26 @@
 package com.devfest.dagger.test;
 
+import android.app.Application;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
 import android.widget.ListView;
 
 import com.devfest.dagger.app.MainActivity;
 import com.devfest.dagger.app.R;
+import com.devfest.dagger.data.DataProvider;
+import com.devfest.dagger.test.mock.MockDataSource;
 import com.devfest.dagger.test.mock.MockMainApplication;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
 
 /**
  * Created by david on 4/28/14.
  */
+
+
+
 public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
 
 
@@ -23,7 +33,9 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
     {
         super.setUp();
 
-        setApplication(new MockMainApplication());
+        Application app = new MockMainApplication();
+        setApplication(app);
+        app.onCreate();
 
     }
 
--- DaggerDemo/app/src/androidTest/java/com/devfest/dagger/test/mock/MockMainApplication.java
+++ DaggerDemo/app/src/androidTest/java/com/devfest/dagger/test/mock/MockMainApplication.java
@@ -1,16 +1,28 @@
 package com.devfest.dagger.test.mock;
 
+import android.test.mock.MockApplication;
+
+import com.devfest.dagger.app.DataModule;
 import com.devfest.dagger.app.MainApplication;
 import com.devfest.dagger.data.DataProvider;
+
+import dagger.ObjectGraph;
 
 /**
  * Created by david on 4/28/14.
  */
 public class MockMainApplication extends MainApplication {
 
-//    @Override
-//    public DataProvider getDataProvider() {
-//        return new MockDataSource();
-//    }
+    private ObjectGraph objectGraph;
+
+    @Override
+    public void onCreate() {
+
+        objectGraph = ObjectGraph.create(new MockModule());
+    }
+
+    public ObjectGraph getObjectGraph() {
+        return this.objectGraph;
+    }
 
 }
